b51bd755f26230cc2f363a35f1c55be53b3bd7d4
alexpondaven <alex.pondaven@gmail.com>
2021-02-27 13:29:04 +0000

    faster genre classification (every 0.5 seconds)

--- genre_classify_mic.py
+++ genre_classify_mic.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 import numpy as np
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import seaborn as sns
 
 import librosa
@@ -17,6 +16,7 @@ import librosa.display
 import pyaudio
 import time
 import wave
+import soundfile as sf
 
 #load model
 model = tf.keras.models.load_model("model80.h5")
@@ -26,8 +26,11 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 1024
-RECORD_SECONDS = 3
-WAVE_OUTPUT_FILENAME = "file.wav"
+RECORD_SECONDS = 0.5
+WAVE_OUTPUT_FILENAME = "out.wav"
+
+genres = 'blues classical country disco hiphop jazz metal pop reggae rock'
+genres = genres.split()
  
 audio = pyaudio.PyAudio()
 #select input device
@@ -39,12 +42,16 @@ for i in range(0, numdevices):
 
 # Above code outputs the index of all input devices
 # Change this parameter to the index of the input device you would like to take the input from
-device_index=1
+device_index= int(input("Index of input device (e.g. 1,2,...): "))
+
 
 # start Recording
 stream = audio.open(format=FORMAT, channels=CHANNELS,
                 rate=RATE, input=True,
                 frames_per_buffer=CHUNK, input_device_index=device_index)
+
+
+
 
 plt.figure(figsize=(10,5))
 #loop for recording 3 second samples from microphone and classifying genre
@@ -57,7 +64,7 @@ while(1):
         frames.append(data)
     # print("finished recording")
 
-    # classify frames
+    # Put frames into wave file
     waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     waveFile.setnchannels(CHANNELS)
     waveFile.setsampwidth(audio.get_sample_size(FORMAT))
@@ -65,11 +72,23 @@ while(1):
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
 
-    t, rate = librosa.load("./file.wav", duration=3) # returns sample rate and data (sig)
-    audio_spect = librosa.feature.melspectrogram(t, rate) # convert to melspectogram
+    # create new 3 second file using 2.5 seconds of old audio sample and 0.5 seconds of new data
+    old_sound, rate = librosa.load("./file.wav", duration=3) # need to have a 3 second wav file in ./file.wav to update
+    #print('shape of old_sound ==> ' + str(old_sound.shape))
+    recorded_sound, rate = librosa.load("./out.wav", duration=RECORD_SECONDS)
+    #print('shape of recorded_sound ==> ' + str(recorded_sound.shape))
+    old_sound = old_sound[-int((3-RECORD_SECONDS)*rate):-1]
+    new_sound = np.append(old_sound, recorded_sound)
+    # extend data to make it 3 seconds
+    #print("rate",rate)
+    #print(new_sound[0:50])
+    new_sound = np.pad(new_sound, (3*rate - len(new_sound),0))
+    #print('shape of old_sound+recorded_sound ==> ' + str(new_sound.shape))
+    sf.write("./file.wav", new_sound, rate)
 
-    genres = 'blues classical country disco hiphop jazz metal pop reggae rock'
-    genres = genres.split()
+    # do classification on file.wav
+    #t, rate = librosa.load("./file.wav", duration=3) # returns sample rate and data (sig)
+    audio_spect = librosa.feature.melspectrogram(new_sound, rate) # convert to melspectogram
 
     spect = audio_spect.reshape(1,128,130,1) / 255.0
     
